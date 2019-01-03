24b300b633a994bb767e1b422587f4eb96f27a1d
Patrick Dill <jamespatrickdill@gmail.com>
2019-01-03 12:20:07 -0500

    google analytics

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -14,5 +14,15 @@ def css(path):
     return send_from_directory("../static", path)
 
 
+# add google analytics code
+
+ANALYTICS = app.config.get("ANALYTICS")
+if ANALYTICS:
+    @app.context_processor
+    def inject_analytics():
+        return dict(analytics=ANALYTICS)
+
+###
+
 if __name__ == "__main__":
     app.run("localhost", 3000, debug=True)
